d42d3a53e6747d256bea7216ed8399e69be6d2d7
Julio Weeb <88756350+julioweeb@users.noreply.github.com>
2025-09-30 17:44:16 -0300

    Add files via upload

---
+++ AtividadeProduto/Produto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace AtividadeProduto
+{
+    class Produto
+    {
+        public String Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque() 
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionaProdutos(int quantidade) 
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome 
+                + ", $ " 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
