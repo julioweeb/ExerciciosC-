f08d5bf6297bbf358ad7f463b6ab98555eef18a2
Julio Weeb <88756350+julioweeb@users.noreply.github.com>
2025-09-30 17:49:59 -0300

    Add files via upload

---
+++ ImparOrPar/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ImparOrPar 
+{
+    public class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Digite um numero: ");
+            int N = int.Parse(Console.ReadLine());
+
+            if (N % 2 == 0) {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+
+        }
+    }
+}
